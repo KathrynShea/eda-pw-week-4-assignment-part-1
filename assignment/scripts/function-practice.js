eee173edbbbd8e7ff7e22e0abd8c9cb837c81d73
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-11-28 18:50:55 -0600

    1-3

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,15 +15,18 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return  'Hello, ' + name + "!";
 }
 // Remember to call the function to test
+console.log ('Test - should say "Hello, Kathryn!"', helloName('Kathryn'));
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+
   // return firstNumber + secondNumber;
-}
+}console.log('Test should be "10"', addNumbers( 7 , 3));
 
 
 // 4. Function to multiply three numbers & return the result
