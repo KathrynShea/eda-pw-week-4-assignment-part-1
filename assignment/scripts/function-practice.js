f04e9f8b0eda098ba0a417e621e01df0a92d0e29
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-11-28 19:05:22 -0600

    1-7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -26,22 +26,23 @@ function addNumbers( firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 
   // return firstNumber + secondNumber;
-}console.log('Test should be "10"', addNumbers( 7 , 3));
+}console.log('Test - should be "10"', addNumbers( 7 , 3));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( firstNum, secondNum, thirdNum){
+  return firstNum * secondNum * thirdNum;
 }
 
+console.log('Test - should be "30"', multiplyThree(5, 2, 3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+    return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -53,15 +54,31 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  return array.at(-1);
 }
+let myArray = ['cat', 'dog', 'fish', 'toad'];
+console.log('Test - should say "toad"', getLast(myArray));
+let emptyArray = [];
+console.log('Test - should say "undefined"', getLast(emptyArray));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  for (thing of array){
+    if (thing === value){
+      return true;
+    }
+  }return false;
 }
+console.log('Test - should say "true"', find('cat', myArray));
+console.log('Test - should say "true"', find('dog', myArray));
+console.log('Test - should say "true"', find('fish', myArray));
+console.log('Test - should say "true"', find('toad', myArray));
+console.log('Test - should say "false"', find('apple', myArray));
+
+
+
 
 // ----------------------
 // Stretch Goals
